fd6740c05f3c4f570d4e391afbd264cb5ac10564
Andrian Syah putra <andriansyahputra260@gmail.com>
2023-09-24 21:01:49 +0700

    wip script

--- script.sh
+++ script.sh
@@ -5,9 +5,6 @@ pkg install python
 pkg install python-pip
 pkg install wget git
 
-wget https://huggingface.co/AndriLawrence/gpt2-chatkobi-ai/resolve/main/1SdjAt39Mjfi2mklO.bin
-wget https://huggingface.co/AndriLawrence/gpt2-chatkobi-ai/resolve/main/1SdjAt39Mjfi2mklO.meta
-
 git clone --recurse-submodules https://github.com/rustformers/llm.git
 cd llm
 cargo build --release

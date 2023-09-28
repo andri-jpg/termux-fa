e4dfa175c13eeb8f2ee6f392222ca7d73019a262
Andrian Syah putra <andriansyahputra260@gmail.com>
2023-09-28 23:48:09 +0700

    ngobrol.py

--- ngobrol.py
+++ ngobrol.py
@@ -7,30 +7,56 @@ from llm_rs import SessionConfig, GenerationConfig, Gpt2
 
 
 class Chainer:
-    def __init__(self):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Chainer, cls).__new__(cls)
+            cls._instance.init_chainer()
+        return cls._instance
+
+    def init_chainer(self):
         self.stop_words = ['<EOL>', '<eol>', '<Eol>', 'pertanyaan :', 'Human', 'human', 'Pertanyaan', '\n']
-        
+        self.previous_qa = []
+
         session_config = SessionConfig(
             threads=4,
-            context_length=1300,
+            context_length=5900,
             prefer_mmap=False
         )
 
         self.generation_config = GenerationConfig(
-            top_p=0.44,
-            top_k=1,
-            temperature=0.22,
-            max_new_tokens=120,
+            top_p=0.88,
+            top_k=8,
+            temperature=0.82,
+            max_new_tokens=600,
             repetition_penalty=1.13,
             stop_words=self.stop_words
         )
 
-        self.model = Gpt2("2midguifSfFt5SbHJsxP.bin", session_config=session_config)
+        self.model = Gpt2("ngobrol.bin", session_config=session_config)
 
     def chain(self, user_input):
-        template = f"saya bisa menjawab pertanyaan dengan masalah kesehatan.\nHai! Saya adalah chatbot yang akan menjawab pertanyaan seputar kesehatan. Saya adalah chatbot, bukan manusia.\nanda dapat menanyakan saya pertanyaan seputar kesehatan melalui kolom teks dibawah.\nPertanyaan saya : {user_input}.\nJawaban anda :"
+        if self.previous_qa:
+            previous_question, previous_answer = self.previous_qa[-1]
+        else:
+            previous_question, previous_answer = "", ""
+
+        template = f"User:\n{previous_question}\n\nAI:\n{previous_answer}\n\nUser: {user_input}.\nAI:"
+
         result = self.model.generate(template, generation_config=self.generation_config)
-        return result.text.strip()
+        response = result.text.strip()
+
+        self.previous_qa.append((user_input, response))
+
+        if len(self.previous_qa) > 4:
+            self.previous_qa.pop(0)
+
+        return response
+
+
+generator = Chainer()
+
 
 
 origins = ["*"] 
